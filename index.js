b72147b9bc00c0cf5bba719eabb029c25bc01be8
mcaeiro <miguel.caeiro@gmail.com>
2018-09-22 21:00:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ client.on('message', msg => {
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.CLIENT_TOKEN);
