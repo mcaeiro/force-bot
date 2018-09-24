3bc9ba7e9d50c0b868ae589d89d899b07e197472
Miguel Caeiro <miguel.caeiro@openbet.com>
2018-09-24 16:53:08 +0100

    Tests

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('Do or do not, there is no try', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
